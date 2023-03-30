5dacb0fcc715b884526e49bdd013f123d2acf12a
12roshan12 <roshansharma7501@gmail.com>
2023-03-30 16:20:41 +0545

    chnages

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {  UserDeleteController, GetAllUserController, GetUserByIdController,NewUserController } = require('../controllers/User.controller')
+const {  UserDeleteController, GetAllUserController, GetUserByIdController,NewUserController } = require('../controllers/user.controller')
 
 router.get('/get-all', GetAllUserController)
 router.post('/', NewUserController)
